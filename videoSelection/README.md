15599746aeaf53c3d5a5e0a51f5f8dd36d5e48f2
Raúl Hernández <raulhdatascientist@gmail.com>
2023-01-14 09:57:47 +0100

    update video selection

--- videoSelection/README.md
+++ videoSelection/README.md
@@ -2,4 +2,4 @@
 
 Part of the project involved analyzing visual regions that processed low-level visual features. To reduce the possibility of having a visual property covariating with one of the categories, I decided to create sets of videos for each category that were balanced
 
-After collecting videos that included cats, dogs, humans or cars. I cut the videos into short fragments that lasted between 3.5, 4.5 and 5.5 s. To reduce the possibility of having a category unbalanced in some low-level visual property.
+After collecting videos that included cats, dogs, humans or cars. I cut the videos into short fragments that lasted between 3.5 and 5.5 s. To reduce the possibility of having a category unbalanced in some low-level visual property, I measured the hue, saturation, brightness, contrast and motion of every video. This is the code to do that (/calculateVideoValues.m).
---
+++ videoSelection/calculateVideoValues.m
@@ -0,0 +1,52 @@
+function [results] = calculateVideoValues(videoName,blockSize,testing)
+%{
+Returns results, a structure with the fields described below. Each property
+represents the average across all frames, the properties calculated are: 
+.hue: hue calculated as the first element in the MATLAB transformation 
+from RGB to HSV
+.saturation: saturation calculated as the second element in the MATLAB 
+transformation from RGB to HSV
+.brightness: brightness calculated by transforming each frame to a grey 
+scale and calculating the average intensity across the image
+.contrast: contrast defined as the root mean square of the grey scale frame
+.motion: motion calculated using blockMatcher a MATLAB method to estimate
+changes in movement between two frames
+.numberOfFrames: number of frames in the video
+%}
+
+%reading the file
+videoObject = VideoReader(videoName);
+numberOfFrames = videoObject.NumFrames;
+videoObject = VideoReader(videoName);
+
+%Initializing variables
+hue = zeros(1,numberOfFrames);
+saturation = zeros(1,numberOfFrames);
+brightness = zeros(1,numberOfFrames);
+contrast = zeros(1,numberOfFrames);
+for j = 1:numberOfFrames %runs through every frame and calculates the values
+    
+    img1 = readFrame(videoObject);
+    img2 = rgb2hsv(img1);
+    hue(j) = mean2(img2(:,:,1));
+    saturation(j) = mean2(img2(:,:,2));
+    imgGray = rgb2gray(img1);
+    brightness(j) = mean2(imgGray);
+    contrast(j) = rms(imgGray(:));%defined as root mean square
+end
+%averaging across frames
+hue = mean(hue);
+saturation = mean(saturation);
+brightness = mean(brightness);
+contrast = mean(contrast);
+
+%Calculates motion using MATLAB's BlockMatcher function
+motion = calculateMotion(videoName,numberOfFrames,blockSize,testing); 
+
+%Writing properties in the results structure
+results.hue = hue;
+results.saturation = saturation;
+results.brightness = brightness;
+results.contrast = contrast;
+results.motion = motion;
+results.numberOfFrames = numberOfFrames;
